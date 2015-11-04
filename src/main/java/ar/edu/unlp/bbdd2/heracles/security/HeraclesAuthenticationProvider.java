85b330e8a243550aafb06229eef3576fd927768a
nhldz <nahd85@gmail.com>
2015-11-04 02:56:11 -0300

    Se reorganiza el AuthenticationProvider. Se crea UserPrincipal que se utiliza para guarda datos del usuario en session.

--- src/main/java/ar/edu/unlp/bbdd2/heracles/security/HeraclesAuthenticationProvider.java
+++ src/main/java/ar/edu/unlp/bbdd2/heracles/security/HeraclesAuthenticationProvider.java
@@ -1,4 +1,4 @@
-package ar.edu.unlp.bbdd2.heracles.helper;
+package ar.edu.unlp.bbdd2.heracles.security;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,12 +10,16 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
+import ar.edu.unlp.bbdd2.heracles.dao.impl.RoleDAOImpl;
+import ar.edu.unlp.bbdd2.heracles.dao.impl.TrainerDAOImpl;
 import ar.edu.unlp.bbdd2.heracles.dao.impl.UserDAOImpl;
 import ar.edu.unlp.bbdd2.heracles.entities.User;
 
 public class HeraclesAuthenticationProvider implements AuthenticationProvider {
 	
 	private UserDAOImpl userDAO;
+	private RoleDAOImpl roleDAO;
+	private TrainerDAOImpl trainerDAO;
 
 	@Override
 	public Authentication authenticate(Authentication authentication)
@@ -23,12 +27,13 @@ public class HeraclesAuthenticationProvider implements AuthenticationProvider {
 		String name = authentication.getName();
 		String password = authentication.getCredentials().toString();
 		User user = this.getUserDAO().loadByName(name);
+		UserPrincipal up = new UserPrincipal(user);
 		Authentication auth = null;
 		if (user != null){
 			if (user.getName().equals(name) && user.getPassword().equals(password)){
 				List<GrantedAuthority> grantedAuths = new ArrayList<>();
-	            grantedAuths.add(new SimpleGrantedAuthority("ROLE_USER"));
-	            auth = new UsernamePasswordAuthenticationToken(name, password, grantedAuths);
+	            grantedAuths.add(new SimpleGrantedAuthority(user.getRoles().get(0).getName()));
+	            auth = new UsernamePasswordAuthenticationToken(up, password, grantedAuths);
 			}
 		}
 		return auth;
@@ -46,6 +51,22 @@ public class HeraclesAuthenticationProvider implements AuthenticationProvider {
 
 	public void setUserDAO(UserDAOImpl userDAO) {
 		this.userDAO = userDAO;
+	}
+
+	public RoleDAOImpl getRoleDAO() {
+		return roleDAO;
+	}
+
+	public void setRoleDAO(RoleDAOImpl roleDAO) {
+		this.roleDAO = roleDAO;
+	}
+
+	public TrainerDAOImpl getTrainerDAO() {
+		return trainerDAO;
+	}
+
+	public void setTrainerDAO(TrainerDAOImpl trainerDAO) {
+		this.trainerDAO = trainerDAO;
 	}
 	
 }
