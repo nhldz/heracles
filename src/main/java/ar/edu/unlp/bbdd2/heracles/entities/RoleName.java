ee3ae3226718db4b36d6cb04c1fe86c1a69e4c3b
nhldz <nahd85@gmail.com>
2015-11-04 02:53:37 -0300

    Enum que contiene los distintos tipos de roles.

---
+++ src/main/java/ar/edu/unlp/bbdd2/heracles/entities/RoleName.java
@@ -0,0 +1,27 @@
+package ar.edu.unlp.bbdd2.heracles.entities;
+
+public enum RoleName {
+	
+	CLIENT ("ROLE_CLIENT", "Rol cliente"),
+	TRAINER ("ROLE_TRAINER", "Rol entrenador"),
+	ADMIN ("ROLE_ADMIN", "Rol administrador");
+	
+	private final String type;
+	private final String description;
+	
+	RoleName (String type, String shortType){
+		this.type = type;
+		this.description = shortType;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+	
+	
+
+}
