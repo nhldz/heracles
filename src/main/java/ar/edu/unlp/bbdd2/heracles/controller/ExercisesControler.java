3774dc7f5f2fae926bbfc2d8b200d3cb65c4bc74
nhldz <nahd85@gmail.com>
2015-11-04 02:49:14 -0300

    Se agrego la eliminacion de ejercicio "delete"

--- src/main/java/ar/edu/unlp/bbdd2/heracles/controller/ExercisesControler.java
+++ src/main/java/ar/edu/unlp/bbdd2/heracles/controller/ExercisesControler.java
@@ -10,10 +10,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import com.google.appengine.repackaged.com.google.gson.Gson;
+import com.google.appengine.repackaged.com.google.gson.GsonBuilder;
 
 import ar.edu.unlp.bbdd2.heracles.bo.impl.BusinessException;
 import ar.edu.unlp.bbdd2.heracles.bo.impl.ExerciseBOImpl;
@@ -21,11 +26,6 @@ import ar.edu.unlp.bbdd2.heracles.bo.impl.TrainerBOImpl;
 import ar.edu.unlp.bbdd2.heracles.entities.Exercise;
 import ar.edu.unlp.bbdd2.heracles.entities.Trainer;
 import ar.edu.unlp.bbdd2.heracles.helper.DataTableObject;
-
-import com.google.appengine.api.users.UserService;
-import com.google.appengine.api.users.UserServiceFactory;
-import com.google.appengine.repackaged.com.google.gson.Gson;
-import com.google.appengine.repackaged.com.google.gson.GsonBuilder;
 
 
 @Controller
@@ -36,9 +36,20 @@ public class ExercisesControler {
 	@Autowired
 	private TrainerBOImpl trainerBO;
 	
+	/**
+	 * Pagina prinicipal de ejercicios.
+	 * @return
+	 */
+	@RequestMapping(value = "/exercises", method = RequestMethod.GET)
+	public ModelAndView getExercises (){
+		ModelAndView mv = new ModelAndView("exercises/list");
+		mv.addObject("exercisesJson",this.exercisesJson(this.exerciseBO.getAllExercises()));
+		return mv;
+	}
+	
 	@RequestMapping(value = "/exercises/{name}", method = RequestMethod.GET)
 	public ModelAndView showMessage(
-			@RequestParam(value = "name") String name) {
+			@PathVariable("name") String name) {
 		System.out.println("in controller exercise "+name);
  
 		ModelAndView mv = new ModelAndView("exercise");
@@ -46,31 +57,41 @@ public class ExercisesControler {
 		return mv;
 	}
 	
-	@RequestMapping(value = "/exercises", method = RequestMethod.GET)
-	public ModelAndView getExercises (){
-		ModelAndView mv = new ModelAndView("trainer/exercises");
-//		mv.addObject("exercises",this.exerciseBO.getAllExercises());
-		mv.addObject("exercisesJson",this.exercisesJson(this.exerciseBO.getAllExercises()));
+//	@RequestMapping(value = "/exercises/{id}", method = RequestMethod.GET)
+//	public ModelAndView getExerciseById (
+//			@RequestParam(value = "id") String id){
+//		ModelAndView mv = new ModelAndView("exercise");
+//		mv.addObject("exercise", this.getExerciseBO().getExerciseDAO().loadById(new Long(id)));
+//		return mv;
+//	}
+	
+	/**
+	 * Elimina un ejercicio
+	 * @param id
+	 * 		id del ejercicio a eliminar.
+	 * @return
+	 */
+	@RequestMapping(value = "/exercises/{id}", method = RequestMethod.DELETE)
+	public ModelAndView delete (
+			@PathVariable("id") String id){
+		Long idL = new Long(id);
+		ModelAndView mv = new ModelAndView("exercises/list");
+		this.getExerciseBO().deleteExercise(idL);
 		return mv;
 	}
 	
 	@RequestMapping(value = "/exercises/{id}", method = RequestMethod.GET)
-	public ModelAndView getExerciseById (
-			@RequestParam(value = "id") String id){
-		ModelAndView mv = new ModelAndView("exercise");
-		mv.addObject("exercise", this.getExerciseBO().getExerciseDAO().loadById(new Long(id)));
-		return mv;
-	}
-	
-	@RequestMapping(value = "/editar", method = RequestMethod.POST)
-	public void editar(HttpServletResponse response, @RequestParam("id") Long id) throws IOException {
+	public void getExerciseById(HttpServletResponse response, @PathVariable("id") String id) throws IOException {
 		response.setContentType("application/json");
+		Long idL = new Long(id);
 		PrintWriter out = response.getWriter();  
-		Exercise exc = this.getExerciseBO().getExerciseDAO().loadById(id);
+		Exercise exc = this.getExerciseBO().getExerciseDAO().loadById(idL);
 		Gson gson = new GsonBuilder().setPrettyPrinting().create();
 		String json = gson.toJson(exc);
 		out.print(json);
 	}
+	
+	//TODO: edit de ejercicios, ver como hacerlos REST.
 	
 	@RequestMapping(value = "exercise", method = RequestMethod.POST)
 	public String add (@ModelAttribute Exercise exercise, Model model){
@@ -93,7 +114,7 @@ public class ExercisesControler {
 	}
 	
 	@RequestMapping(value = "/getExercises", method = RequestMethod.GET)
-	public void printHello(HttpServletResponse response) throws IOException {
+	public void getExercises(HttpServletResponse response) throws IOException {
 		response.setContentType("application/json");
 		PrintWriter out = response.getWriter();
 		
