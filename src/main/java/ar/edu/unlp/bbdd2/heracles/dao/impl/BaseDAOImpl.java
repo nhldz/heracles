c03912a548e3dba1d5de162a9f87b428f4ac87af
nhldz <nahd85@gmail.com>
2015-11-04 02:49:41 -0300

    Se agrego método COUNT

--- src/main/java/ar/edu/unlp/bbdd2/heracles/dao/impl/BaseDAOImpl.java
+++ src/main/java/ar/edu/unlp/bbdd2/heracles/dao/impl/BaseDAOImpl.java
@@ -5,12 +5,12 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
-import ar.edu.unlp.bbdd2.heracles.dao.BaseDAO;
-
 import com.googlecode.objectify.LoadResult;
 import com.googlecode.objectify.Objectify;
 import com.googlecode.objectify.ObjectifyFactory;
 import com.googlecode.objectify.ObjectifyService;
+
+import ar.edu.unlp.bbdd2.heracles.dao.BaseDAO;
 
 public abstract class BaseDAOImpl<T> implements BaseDAO<T> {
 	
@@ -29,30 +29,47 @@ public abstract class BaseDAOImpl<T> implements BaseDAO<T> {
     }
 
     public static ObjectifyFactory factory() {
-        return ObjectifyService.factory();
+    	ObjectifyFactory factory = ObjectifyService.factory();
+    	factory.setSaveWithNewEmbedFormat(true);
+        return factory;
     }
-
+    
+    /**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void save(T object) {
 		ofy().save().entity(object).now();
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void saveAll(Collection<T> objects) {
 		ofy().save().entities(objects).now();		
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void saveOrUpdate(T object) {
 		ofy().save().entity(object).now();		
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void saveOrUpdateAll(Collection<T> objects) {
 		ofy().save().entities(objects).now();		
 		
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public T load(T object) {
 		LoadResult<T> resutl = ofy().load().entity(object);
@@ -60,17 +77,26 @@ public abstract class BaseDAOImpl<T> implements BaseDAO<T> {
 		return resutl.now();
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public T loadById(Long id) {
 		return ofy().load().type(this.getType()).id(id).now();
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public Collection<T> loadAll(Collection<T> objects) {
 		List<T> listResult = ofy().load().type(this.getType()).list();
 		return listResult;
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public Collection<T> loadAllById(Collection<Long> ids) {
 		Map<Long, T> mapResult = ofy().load().type(this.getType()).ids(ids);
@@ -81,24 +107,44 @@ public abstract class BaseDAOImpl<T> implements BaseDAO<T> {
 		return (List<T>) ofy().load().type(this.getType()).list();
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void delete(T object) {
 		ofy().delete().entity(object);
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void deleteById(Long id) {
 		ofy().delete().type(this.getType()).id(id);
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void deleteAll(Collection<T> objects) {
 		ofy().delete().entities(objects);
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void deleteAllByIds(Collection<Long> ids) {
 		ofy().delete().type(this.getType()).ids(ids);
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public int count() {
+		return ofy().load().type(this.getType()).count();
 	}
 
 }
