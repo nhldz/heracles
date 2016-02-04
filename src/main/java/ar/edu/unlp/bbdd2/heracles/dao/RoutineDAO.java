19b67bbcaf28ebfd3a21e822ee6a0a5924d2e9fa
nhldz <nahd85@gmail.com>
2016-02-03 23:38:21 -0300

    Se obtienen rutinas de un cliente.

--- src/main/java/ar/edu/unlp/bbdd2/heracles/dao/RoutineDAO.java
+++ src/main/java/ar/edu/unlp/bbdd2/heracles/dao/RoutineDAO.java
@@ -2,6 +2,7 @@ package ar.edu.unlp.bbdd2.heracles.dao;
 
 import java.util.List;
 
+import ar.edu.unlp.bbdd2.heracles.entities.Client;
 import ar.edu.unlp.bbdd2.heracles.entities.Routine;
 import ar.edu.unlp.bbdd2.heracles.entities.Trainer;
 
@@ -13,5 +14,12 @@ import ar.edu.unlp.bbdd2.heracles.entities.Trainer;
 public interface RoutineDAO extends BaseDAO<Routine>{
 
 	List<Routine> getTrainerRoutines(Trainer trainer);
+	
+	/**
+	 * Rutinas de un cliente
+	 * @param client
+	 * @return
+	 */
+	List<Routine> getClientRoutines(Client client);
 
 }
