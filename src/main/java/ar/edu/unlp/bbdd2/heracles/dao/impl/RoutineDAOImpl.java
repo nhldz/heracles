faac9db852432d38b63c30345d58b590b629622b
nhldz <nahd85@gmail.com>
2016-02-03 23:39:30 -0300

    Se obtiene las rutinas de un cliente.

--- src/main/java/ar/edu/unlp/bbdd2/heracles/dao/impl/RoutineDAOImpl.java
+++ src/main/java/ar/edu/unlp/bbdd2/heracles/dao/impl/RoutineDAOImpl.java
@@ -3,6 +3,7 @@ package ar.edu.unlp.bbdd2.heracles.dao.impl;
 import java.util.List;
 
 import ar.edu.unlp.bbdd2.heracles.dao.RoutineDAO;
+import ar.edu.unlp.bbdd2.heracles.entities.Client;
 import ar.edu.unlp.bbdd2.heracles.entities.Routine;
 import ar.edu.unlp.bbdd2.heracles.entities.Trainer;
 
@@ -21,6 +22,12 @@ public class RoutineDAOImpl extends BaseDAOImpl<Routine> implements RoutineDAO {
 	@Override
 	public List<Routine> getTrainerRoutines(Trainer trainer) {
 		List<Routine> routines = ofy().load().type(this.getType()).filter("trainer", trainer).list();
+		return routines;
+	}
+
+	@Override
+	public List<Routine> getClientRoutines(Client client) {
+		List<Routine> routines = ofy().load().type(this.getType()).filter("client", client).list();
 		return routines;
 	}
 }
