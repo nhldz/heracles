308bd83495d2d6ef481a08530e598360fe55008a
nhldz <nahd85@gmail.com>
2016-05-06 00:15:23 -0300

    Se mejora el constructor

--- src/main/java/ar/edu/unlp/bbdd2/heracles/entities/Activity.java
+++ src/main/java/ar/edu/unlp/bbdd2/heracles/entities/Activity.java
@@ -3,12 +3,11 @@ package ar.edu.unlp.bbdd2.heracles.entities;
 import java.util.ArrayList;
 import java.util.List;
 
-import ar.edu.unlp.bbdd2.heracles.helper.RefHelper;
-
 import com.googlecode.objectify.Ref;
 import com.googlecode.objectify.annotation.Entity;
 import com.googlecode.objectify.annotation.Id;
-import com.googlecode.objectify.annotation.Load;
+
+import ar.edu.unlp.bbdd2.heracles.helper.RefHelper;
 
 /**
  *
@@ -28,13 +27,13 @@ public class Activity {
 	private Ref<Routine> routine;
 	
 	//Ejercicio que se esta haciendo actualemnte
-	@Load
 	private Ref<ExerciseConfiguration> runExercise;
 //	private Activity nextActivity;
 //	private Activity previousActivity;
 	private List<Ref<ExerciseConfiguration>> exercises;
 	
 	public Activity() {
+		super();
 		this.exercises = new ArrayList<Ref<ExerciseConfiguration>>();
 	}
 	
