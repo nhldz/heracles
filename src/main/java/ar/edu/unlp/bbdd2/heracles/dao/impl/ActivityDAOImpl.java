1881379f0ec04f00df1e535e8d2a243da841afd5
nhldz <nahd85@gmail.com>
2016-05-10 01:58:32 -0300

    Se agregan interfaces para ActivityDAO y ExerciseConfigurationDAO, se incorrporan nuevos metodos a los DAO particulares de cada DAO

--- src/main/java/ar/edu/unlp/bbdd2/heracles/dao/impl/ActivityDAOImpl.java
+++ src/main/java/ar/edu/unlp/bbdd2/heracles/dao/impl/ActivityDAOImpl.java
@@ -1,12 +1,43 @@
 package ar.edu.unlp.bbdd2.heracles.dao.impl;
 
-import ar.edu.unlp.bbdd2.heracles.entities.Activity;
+import java.util.ArrayList;
+import java.util.List;
 
-public class ActivityDAOImpl extends BaseDAOImpl<Activity>{
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+
+import ar.edu.unlp.bbdd2.heracles.dao.ActivityDAO;
+import ar.edu.unlp.bbdd2.heracles.entities.Activity;
+import ar.edu.unlp.bbdd2.heracles.entities.ExerciseConfiguration;
+import ar.edu.unlp.bbdd2.heracles.entities.ExerciseSnapshot;
+import ar.edu.unlp.bbdd2.heracles.entities.ExerciseState;
+
+public class ActivityDAOImpl extends BaseDAOImpl<Activity> implements ActivityDAO{
+	
+	private DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 	
 	public ActivityDAOImpl (Class<Activity> classType){
 		super();
 		this.type = classType;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Integer excerciseStateCount(Activity activity, ExerciseState state) {
+		List<ExerciseConfiguration> exConfs = new ArrayList<ExerciseConfiguration>();
+		exConfs.addAll(activity.getExercises());
+
+		return ofy().load().type(ExerciseSnapshot.class).filter("exerciseConfiguration IN", exConfs).filter("state =", state).count();
+	}
+
+	public DatastoreService getDatastore() {
+		return datastore;
+	}
+
+	public void setDatastore(DatastoreService datastore) {
+		this.datastore = datastore;
 	}
 	
 }
--- src/main/java/ar/edu/unlp/bbdd2/heracles/dao/impl/ExerciseConfigurationDAOImpl.java
+++ src/main/java/ar/edu/unlp/bbdd2/heracles/dao/impl/ExerciseConfigurationDAOImpl.java
@@ -1,12 +1,27 @@
 package ar.edu.unlp.bbdd2.heracles.dao.impl;
 
-import ar.edu.unlp.bbdd2.heracles.entities.ExerciseConfiguration;
+import java.util.ArrayList;
+import java.util.List;
 
-public class ExerciseConfigurationDAOImpl extends BaseDAOImpl<ExerciseConfiguration> {
+import ar.edu.unlp.bbdd2.heracles.dao.ExerciseConfigurationDAO;
+import ar.edu.unlp.bbdd2.heracles.entities.ExerciseConfiguration;
+import ar.edu.unlp.bbdd2.heracles.entities.ExerciseSnapshot;
+
+public class ExerciseConfigurationDAOImpl extends BaseDAOImpl<ExerciseConfiguration> implements ExerciseConfigurationDAO{
 	
 	public ExerciseConfigurationDAOImpl (Class<ExerciseConfiguration> classType){
 		super();
 		this.type = classType;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public ExerciseSnapshot lastSnapshot(ExerciseConfiguration exerciseConfiguration) {
+		List<ExerciseConfiguration> exConfs = new ArrayList<ExerciseConfiguration>();
+		exConfs.add(exerciseConfiguration);
+		return ofy().load().type(ExerciseSnapshot.class).filter("exerciseConfiguration IN", exConfs).order("-endDate").first().now();
 	}
 
 }
